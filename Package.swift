fd459c8060d46d17b780a80ac43db83b444e2e13
Darren Ford <dford_au-reg@yahoo.com>
2025-02-23 15:08:34 +1100

    Added privacy manifest

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.4
 
 import PackageDescription
 
@@ -9,21 +8,19 @@ let package = Package(
 		.macOS(.v10_11),
 	],
 	products: [
-		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(name: "DSFAppearanceManager", targets: ["DSFAppearanceManager"]),
 		.library(name: "DSFAppearanceManager-static", type: .static, targets: ["DSFAppearanceManager"]),
 		.library(name: "DSFAppearanceManager-shared", type: .dynamic, targets: ["DSFAppearanceManager"]),
 	],
-	dependencies: [
-		// Dependencies declare other packages that this package depends on.
-		// .package(url: /* package url */, from: "1.0.0"),
-	],
+	dependencies: [],
 	targets: [
-		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
-		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
 			name: "DSFAppearanceManager",
-			dependencies: []),
+			dependencies: [],
+			resources: [
+				.copy("PrivacyInfo.xcprivacy"),
+			]
+		),
 		.testTarget(
 			name: "DSFAppearanceManagerTests",
 			dependencies: ["DSFAppearanceManager"]),
